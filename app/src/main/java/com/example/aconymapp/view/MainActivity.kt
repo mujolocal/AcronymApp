8957dd4e00b23205c8b82d9e576af54a2affb8b6
Muammar Johnson <muammarj@gmail.com>
2021-05-04 21:45:50 -0400

    dependancies and what not added

--- app/src/main/java/com/example/aconymapp/view/MainActivity.kt
+++ app/src/main/java/com/example/aconymapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.aconymapp
+package com.example.aconymapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aconymapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
