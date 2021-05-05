fc191916ddd155279ac9fdece3c5c1e6318b355a
Muammar Johnson <muammarj@gmail.com>
2021-05-05 15:30:54 -0400

    button connected to input text

--- app/src/main/java/com/example/aconymapp/viewModel/SharedViewModel.kt
+++ app/src/main/java/com/example/aconymapp/viewModel/SharedViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.aconymapp.viewModel
 
 import android.util.Log
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,29 +11,31 @@ import com.example.aconymapp.repo.AcronymRepo
 import com.example.aconymapp.util.ApiState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
 
 
 class SharedViewModel : ViewModel(){
     private  val TAG = "SharedViewModel"
+    var acronym = ObservableField<String>()
     private val _apiState = MutableLiveData<ApiState<AcronymResponseItem>>()
     val apiState: LiveData<ApiState<AcronymResponseItem>>
         get() = _apiState
 
-//    fun getResponse(acronym:String) {
-fun getResponse() {
+    fun getResponse() {
         _apiState.postValue(ApiState.Loading)
         try {
             Log.d(TAG, "getResponse: started")
             viewModelScope.launch(Dispatchers.IO) {
+                acronym.get()?.let {
                 _apiState.postValue(ApiState.Loading)
-//                val acronymResponse: AcronymResponse? = AcronymRepo.getAcronymResponse(acronym)
-                val acronymResponse: List<AcronymResponseItem>? = AcronymRepo.getAcronymResponse("fe")
+                val acronymResponse: List<AcronymResponseItem>? = AcronymRepo.getAcronymResponse(it)
                 if (acronymResponse != null) {
                     Log.d(TAG, "getResponse:${acronymResponse} ")
                     _apiState.postValue(ApiState.Success(acronymResponse[0]))
                 }
             }
-        } catch (e: Exception) {
+        }
+        }catch (e: Exception) {
             ApiState.Error("Network error, please try again.")
         }
     }
